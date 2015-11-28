5364c47d8199f25d13e14470745c1ae3d1754e8d
jSheraden <josasheraden@gmail.com>
2015-11-28 01:07:23 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function theme_scripts() {
     wp_register_script( 'jquery', get_stylesheet_directory_uri() . '/bower_components/jquery/dist/jquery.min.js' );
-    wp_register_script( 'bootstrap', get_stylesheet_directory_uri() . '/bower_component/bootstrap/dist/js/bootstrap.min.js' );
+    wp_register_script( 'bootstrap', get_stylesheet_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js' );
     wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery', 'bootstrap' ) );
 }
 

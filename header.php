3a698e2524605ea1b32d29e0edd06111a86d6a84
jSheraden <josasheraden@gmail.com>
2015-11-28 01:05:07 -0600

    Update header.php

--- header.php
+++ header.php
@@ -5,10 +5,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="<?php bloginfo( 'template_url' ); ?>/bower_components/jquery/dist/jquery.min.js"></script>
   <link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <script src="<?php bloginfo( 'template_url' ); ?>/bower_components/bootstrap/dist/js/boostrap.min.js"></script>
-  <script src="<?php bloginfo( 'template_url' ); ?>/js/script.js"></script>
   <link rel="stylesheet" href="style.css">
   <title><?php bloginfo( 'name' ); ?></title>
   <?php wp_head(); ?>

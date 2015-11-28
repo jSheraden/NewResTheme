38dc58c9b936a254608e899f549c46b87d60ff2b
jSheraden <josasheraden@gmail.com>
2015-11-28 00:08:53 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <script src="<?php bloginfo( 'template_url' ); ?>/bower_components/bootstrap/dist/js/boostrap.min.js"></script>
+  <script src="<?php bloginfo( 'template_url' ); ?>/js/script.js"></script>
+  <link rel="stylesheet" href="style.css">
+  <title><?php bloginfo( 'name' ); ?></title>
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div class="container-fluid">
+    <div class="site-header">
+      <div class="site-title">
+        <h1><?php bloginfo( 'name' ); ?></h1>
+      </div>
+      

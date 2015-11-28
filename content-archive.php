c3031912a38ef464564dbf425a75ad1f839102e0
jSheraden <josasheraden@gmail.com>
2015-11-28 03:04:18 -0600

    Update content-archive.php

--- content-archive.php
+++ content-archive.php
@@ -1,4 +1,4 @@
-<article class="post col-xs-12 col-sm-6 col-md-4 col-lg-4">
+<article class="post col-xs-12 col-sm-12 col-md-12 col-lg-12">
   <?php $author_archive_link = get_author_posts_url( get_the_author_meta( "ID" ) ); ?>
   <h3><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
   <h5>

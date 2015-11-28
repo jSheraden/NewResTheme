2a56af3dd0b541f2398397f5ed9dde5e99fade66
jSheraden <josasheraden@gmail.com>
2015-11-28 01:04:28 -0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function theme_scripts() {
+    wp_register_script( 'jquery', get_stylesheet_directory_uri() . '/bower_components/jquery/dist/jquery.min.js' );
+    wp_register_script( 'bootstrap', get_stylesheet_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js' );
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery', 'bootstrap' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
+?>

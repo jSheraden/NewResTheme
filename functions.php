9cd1abe2a0efb69186f92d764a3ff2157c95b410
jSheraden <josasheraden@gmail.com>
2015-12-15 23:12:24 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,12 @@
 <?php
 
-function theme_resources() {
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css' );
-    wp_register_script( 'jquery', get_stylesheet_directory_uri() . '/bower_components/jquery/dist/jquery.min.js' );
-    wp_register_script( 'bootstrap', get_stylesheet_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js' );
-    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery', 'bootstrap' ) );
-}
-
-add_action( 'wp_enqueue_scripts', 'theme_resources' );
-
 $args = array(
 	'theme_location'  => '',
 	'menu'            => '',
 	'container'       => 'div',
 	'container_class' => '',
 	'container_id'    => '',
-	'menu_class'      => 'menu',
+	'menu_class'      => 'menu nav nav-pills',
 	'menu_id'         => '',
 	'echo'            => true,
 	'fallback_cb'     => 'wp_page_menu',
@@ -28,6 +18,16 @@ $args = array(
 	'depth'           => 0,
 	'walker'          => ''
 );
+
+function theme_resources() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css' );
+    wp_register_script( 'jquery', get_stylesheet_directory_uri() . '/bower_components/jquery/dist/jquery.min.js' );
+    wp_register_script( 'bootstrap', get_stylesheet_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js' );
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery', 'bootstrap' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_resources' );
 
 function post_loop( $content_type ) {
     if ( have_posts() ) {

00168a3969e3034c24fb08765395fe22b8355eb9
jSheraden <josasheraden@gmail.com>
2015-12-19 21:24:38 -0700

    Update content.php

--- content.php
+++ content.php
@@ -1,10 +1,10 @@
-<article class="post col-xs-12 col-sm-6 col-md-4 col-lg-4">
+<article class="post post-single col-xs-12 col-sm-6 col-md-4 col-lg-4">
   <?php $author_archive_link = get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>
   <h3><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
   <h5>
     <i>
       Posted by 
-      <a class="author" href="<?php echo $author_archive_link; ?>"><?php the_author(); ?></a> 
+      <a class="author-archive-link" href="<?php echo $author_archive_link; ?>"><?php the_author(); ?></a> 
       on 
       <?php the_time( 'F jS, Y g:i a' ); ?>
     </i>

4d77f6ee6f3a45a105ae4ab4c63ff6b10439145c
winston251 <winstonbartho@gmail.com>
2015-12-19 19:37:08 -0700

    Fixed post info tag to align with post title.

--- content-single.php
+++ content-single.php
@@ -1,12 +1,13 @@
+<?php $author_archive_link = get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>
+<h5>
+  <i>
+    Posted by 
+    <a class="author" href="<?php echo $author_archive_link; ?>"><?php the_author(); ?></a> 
+    on 
+    <?php the_time( 'F jS, Y g:i a' ); ?>
+  </i>
+</h5>
+
 <article class="post col-xs-12 col-sm-12 col-md-12 col-lg-12">
-  <?php $author_archive_link = get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>
-  <h5>
-    <i>
-      Posted by 
-      <a class="author" href="<?php echo $author_archive_link; ?>"><?php the_author(); ?></a> 
-      on 
-      <?php the_time( 'F jS, Y g:i a' ); ?>
-    </i>
-  </h5>
   <?php the_content(); ?>
 </article>
